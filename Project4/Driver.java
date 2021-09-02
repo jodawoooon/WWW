6e79aa36f216b2f78dcf4c9c24c4a2b53fc88575
박종대 <pjx256@gmail.com>
2021-09-02 17:07:57 +0000

    FEATURE: MatrixAdd

---
+++ Project4/Driver.java
@@ -0,0 +1,26 @@
+package ssafy;
+
+import org.apache.hadoop.util.ProgramDriver;
+
+public class Driver {
+	public static void main(String[] args) {
+		int exitCode = -1;
+		ProgramDriver pgd = new ProgramDriver();
+		try {
+
+			pgd.addClass("wordcount", Wordcount.class, "A map/reduce program that performs word counting.");
+			pgd.addClass("wordcount1char", Wordcount1char.class, "A map/reduce program that counts the 1st character of words in the input files.");
+			pgd.addClass("wordcountsort", Wordcountsort.class, "A map/reduce Program that output frequency of the words in the input files by alphabetical order.");
+			pgd.addClass("inverted", InvertedIndex.class, "A map/reduce Program that generates the inverted index using words in the input files.");
+			pgd.addClass("matadd", MatrixAdd.class, "A map/reduce Program that computes the addtion of two matrices.");
+
+      			pgd.driver(args);
+			exitCode = 0;
+		}
+		catch(Throwable e) {
+			e.printStackTrace();
+		}
+
+		System.exit(exitCode);
+	}
+}
