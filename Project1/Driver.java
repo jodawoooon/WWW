f6ddaf6f591d7ef93bd91941526aad69c8aa638f
Daun jo <jodawooooon@naver.com>
2021-09-02 18:03:42 +0900

    feat: :sparkles: 프로젝트1

---
+++ Project1/Driver.java
@@ -0,0 +1,22 @@
+package ssafy;
+
+import org.apache.hadoop.util.ProgramDriver;
+
+public class Driver {
+	public static void main(String[] args) {
+		int exitCode = -1;
+		ProgramDriver pgd = new ProgramDriver();
+		try {
+
+		
+			pgd.addClass("wordcount1char", Wordcount1char.class, "A map/reduce program that counts the 1st character of words in the input files");
+      			pgd.driver(args);
+			exitCode = 0;
+		}
+		catch(Throwable e) {
+			e.printStackTrace();
+		}
+
+		System.exit(exitCode);
+	}
+}
