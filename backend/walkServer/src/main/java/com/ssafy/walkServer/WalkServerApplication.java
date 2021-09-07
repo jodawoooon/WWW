88f8f8315ee881cdb72a8de80d439b65709e0e13
jihyun9623 <jihyun9623@gmail.com>
2021-09-07 15:35:09 +0900

    Create

---
+++ backend/walkServer/src/main/java/com/ssafy/walkServer/WalkServerApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.walkServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WalkServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WalkServerApplication.class, args);
+	}
+
+}
