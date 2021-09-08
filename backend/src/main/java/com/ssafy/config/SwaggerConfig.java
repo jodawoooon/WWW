9f9d1e2ebb41d775181332f65c4b36d320eccc56
Daun jo <jodawooooon@naver.com>
2021-09-08 14:10:35 +0900

    [#1] init: Swagger Config 파일 정의

---
+++ backend/src/main/java/com/ssafy/config/SwaggerConfig.java
@@ -0,0 +1,14 @@
+package com.ssafy.config;
+
+
+import org.springframework.context.annotation.Configuration;
+
+
+/**
+ * API 문서 관련 swagger 설정 정의.
+ * URL - http://localhost:8080/swagger-ui/
+ */
+@Configuration
+public class SwaggerConfig {
+
+}
