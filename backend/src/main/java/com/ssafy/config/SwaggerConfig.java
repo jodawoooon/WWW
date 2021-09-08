c6238b03791b656f9e18c8c953dc17bc2169f610
Daun jo <jodawooooon@naver.com>
2021-09-08 15:25:12 +0900

    [#1] init: 프로젝트 기본 환경설정 완료

--- backend/src/main/java/com/ssafy/config/SwaggerConfig.java
+++ backend/src/main/java/com/ssafy/config/SwaggerConfig.java
@@ -1,14 +1,48 @@
 package com.ssafy.config;
 
-
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.ApiKey;
+import springfox.documentation.service.AuthorizationScope;
+import springfox.documentation.service.SecurityReference;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger.web.UiConfiguration;
+import springfox.documentation.swagger.web.UiConfigurationBuilder;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.List;
+
+import static com.google.common.collect.Lists.newArrayList;
 
 /**
- * API 문서 관련 swagger 설정 정의.
- * URL - http://localhost:8080/swagger-ui/
+ * API 문서 관련 swagger2 설정 정의.
  */
 @Configuration
+@EnableSwagger2
 public class SwaggerConfig {
 
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2).useDefaultResponseMessages(false)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.ant("/api/**"))
+                .build()
+                ;
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("WWW Spring Boot REST API")
+                .version("1.0.0")
+                .description("WWW의 swagger api 입니다.")
+                .build();
+    }
 }
