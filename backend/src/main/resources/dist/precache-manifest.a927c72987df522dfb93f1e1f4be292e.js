5f43f4f303e77906ad1e658d78eeae430faf24d1
jihyun9623 <jihyun9623@gmail.com>
2021-09-14 10:51:28 +0900

    Feat: 젠킨스 테스트 4

---
+++ backend/src/main/resources/dist/precache-manifest.a927c72987df522dfb93f1e1f4be292e.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1fc741f83bf684533388",
+    "url": "/css/app.5ef35f11.css"
+  },
+  {
+    "revision": "badd07c9ebed05d0f4e2",
+    "url": "/css/chunk-76d56bd0.fe62aeb5.css"
+  },
+  {
+    "revision": "3ac3699f0415443022a7",
+    "url": "/css/chunk-vendors.595c620e.css"
+  },
+  {
+    "revision": "8f69d6f59efeb124adfc3ef4fecb26ad",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1fc741f83bf684533388",
+    "url": "/js/app.bf719eca.js"
+  },
+  {
+    "revision": "badd07c9ebed05d0f4e2",
+    "url": "/js/chunk-76d56bd0.eb738f07.js"
+  },
+  {
+    "revision": "3ac3699f0415443022a7",
+    "url": "/js/chunk-vendors.8fe17f90.js"
+  },
+  {
+    "revision": "4b14c64efaf846819b9a229b4193c8b7",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/robots.txt"
+  }
+]);
