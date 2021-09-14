11834a3ead9cc9847f8bda0e05e2592fac1879e0
hyewon <hiii456@daum.net>
2021-09-14 15:16:57 +0900

    [#3] feat: kakao Service 및 Controller 코드 수정

--- backend/src/main/java/com/ssafy/api/controller/KakaoController.java
+++ backend/src/main/java/com/ssafy/api/controller/KakaoController.java
@@ -3,13 +3,10 @@ package com.ssafy.api.controller;
 import com.ssafy.api.response.UserLoginPostRes;
 import com.ssafy.api.service.KakaoAPI;
 import com.ssafy.api.service.UserService;
-import com.ssafy.db.entity.User;
-import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -27,60 +24,15 @@ public class KakaoController {
     @Autowired
     UserService userService;
 
-    @GetMapping(value = "/oauth")
-    public ResponseEntity<String> kakaoConnect(){
-        StringBuffer url = new StringBuffer();
-        url.append("https://kauth.kakao.com/oauth/authorize?");
-        url.append("client_id=" + "1e31c0b3e807829e950f0236c26efec6");
-        url.append("&redirect_uri=http://localhost:8080/kakao/login");
-        url.append("&response_type=code");
-        System.out.println(url.toString());
-        return new ResponseEntity<String>(url.toString(), HttpStatus.OK);
-    }
-
     @RequestMapping(value="/login")
     public ResponseEntity<UserLoginPostRes> login(@RequestParam("code") String code, HttpSession session) {
-        HashMap<String,Object> Token = kakaoAPI.getAccessToken(code);
-
-        System.out.println(Token.toString());
-
-        String access_Token = (String) Token.get("accessToken");
-        String refresh_Token = (String) Token.get("refreshToken");
-
-        System.out.println("code : " + code);
-        // 사용자의 정보를 <string, 객체> 로 생성
-        HashMap<String, Object> userInfo = kakaoAPI.getUserInfo(access_Token, refresh_Token); //front 전송 용 유저 Info Hash Map
-        HashMap<String, Object> userProfile = kakaoAPI.getUserProfile(access_Token, refresh_Token); //create 용 유저 Profile Hash Map
+        String access_Token = kakaoAPI.getAccessToken(code);
+        HashMap<String, Object> userInfo = kakaoAPI.getUserInfo(access_Token);
 
         System.out.println("login Controller : " + userInfo);
 
-        HashMap<String, Object> userObject = new HashMap<String, Object>();
-        userObject.put("Token",Token);
-        userObject.put("userInfo",userInfo);
-
-        System.out.println("-------------------");
-        System.out.println(userObject);
-        System.out.println("-------------------");
-
-        // 카카오가 보낸 정보에서 id를 가져온다.
-        String id = (String) userInfo.get("userid");
-        System.out.println(id);
-        User user = userService.getUserId(id);
-        // 회원가입이 되어있는 경우
-        if(user!=null){
-            System.out.println("login success!");
-            return ResponseEntity.ok(UserLoginPostRes.of(200,"Success", userObject));
-        }
-        // 클라이언트의 이메일이 존재할 때 세션에 해당 이메일과 토큰 등록
-        if (userInfo.get("email") != null) {
-//                session.setAttribute("userId", userInfo.get("email"));
-//                session.setAttribute("access_Token", userObject);
-        }
-        userService.createUser(access_Token, refresh_Token, userProfile);
-        return ResponseEntity.ok(UserLoginPostRes.of(200, "Success", userObject));
-
+        return ResponseEntity.ok(UserLoginPostRes.of(200, "Success", userInfo));
     }
-
 
     @ApiOperation(value = "카카오 계정 로그아웃", notes = "Kakao 인증서버에 accessToken을 넘겨주어 로그인된 카카오 계정을 로그아웃 시킨다.")
     @ApiResponses({
--- backend/src/main/java/com/ssafy/api/service/KakaoAPI.java
+++ backend/src/main/java/com/ssafy/api/service/KakaoAPI.java
@@ -3,6 +3,7 @@ package com.ssafy.api.service;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.io.*;
@@ -13,7 +14,10 @@ import java.util.HashMap;
 @Service
 public class KakaoAPI {
 
-    public HashMap<String, Object> getAccessToken(String authorize_code){
+    @Autowired
+    RedisService redisService;
+
+    public String getAccessToken(String authorize_code){
         String access_Token = "";
         String refresh_Token = "";
 
@@ -33,7 +37,7 @@ public class KakaoAPI {
             StringBuilder sb = new StringBuilder();
             sb.append("grant_type=authorization_code");
             sb.append("&client_id=1e31c0b3e807829e950f0236c26efec6");
-            sb.append("&redirect_uri=https://localhost:8080/kakao/callback");
+            sb.append("&redirect_uri=http://localhost:8080/kakao/login");
             sb.append("&code=" + authorize_code);
             bw.write(sb.toString());
             bw.flush();
@@ -67,76 +71,11 @@ public class KakaoAPI {
         } catch (IOException e) {
             e.printStackTrace();
         }
-        return Token;
-    }
-
-    // UserProfile를 받아오는 메소드(userId, email, profileImage, name, phoneNumber)
-    public HashMap<String, Object> getUserProfile(String access_Token, String refresh_Token){
-        String accessToken = access_Token;
-        String refreshToken = refresh_Token;
-        String userid="";
-        String profileImageUrl = "";
-        String name = "";
-
-        // 요청하는 클라이언트마다 가진 정보가 다를 수 있기에 HashMap타입으로 선언
-        HashMap<String, Object> userInfo = new HashMap<>();
-        String reqURL = "https://kapi.kakao.com/v2/user/me";
-        try {
-            URL url = new URL(reqURL);
-            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-            conn.setRequestMethod("POST");
-
-            // 요청에 필요한 Header에 포함될 내용
-            conn.setRequestProperty("Authorization", "Bearer " + access_Token);
-
-            int responseCode = conn.getResponseCode();
-            System.out.println("responseCode : " + responseCode);
-
-            BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream()));
-
-            String line = "";
-            String result = "";
-
-            while ((line = br.readLine()) != null) {
-                result += line;
-            }
-            System.out.println("response body : " + result);
-
-            JsonParser parser = new JsonParser();
-            JsonElement element = parser.parse(result);
-            JsonObject kakao_account = element.getAsJsonObject().get("kakao_account").getAsJsonObject();
-
-            userid = element.getAsJsonObject().get("id").getAsString();
-            profileImageUrl = kakao_account.getAsJsonObject().get("profile").getAsJsonObject().get("profile_image_url").getAsString();
-            name = kakao_account.getAsJsonObject().get("profile").getAsJsonObject().get("nickname").getAsString();
-
-            JsonElement email = kakao_account.getAsJsonObject().get("email");
-
-            userInfo.put("accessToken", accessToken);
-            userInfo.put("refreshToken", refreshToken);
-            userInfo.put("userid", userid);
-            userInfo.put("profileImageUrl", profileImageUrl);
-            userInfo.put("name", name);
-            userInfo.put("phoneNumber", "");
-            userInfo.put("birthday", "");
-
-            if(email!=null){
-                userInfo.put("email", email.getAsString());
-            }else{
-                userInfo.put("email" , "");
-            }
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return userInfo;
+        return access_Token;
     }
 
     // UserInfo를 받아오는 메소드(userId, email)
-    public HashMap<String, Object> getUserInfo(String accessToken, String refreshToken){
-        String userid="";
-        JsonElement email;
-
+    public HashMap<String, Object> getUserInfo(String accessToken){
         // 요청하는 클라이언트마다 가진 정보가 다를 수 있기에 HashMap타입으로 선언
         HashMap<String, Object> userInfo = new HashMap<>();
         String reqURL = "https://kapi.kakao.com/v2/user/me";
@@ -167,17 +106,12 @@ public class KakaoAPI {
             JsonObject properties = element.getAsJsonObject().get("properties").getAsJsonObject();
             JsonObject kakao_account = element.getAsJsonObject().get("kakao_account").getAsJsonObject();
 
-            userid = element.getAsJsonObject().get("id").getAsString();
-            email = kakao_account.getAsJsonObject().get("email");
+            String userId = element.getAsJsonObject().get("id").getAsString();
+            //String email = kakao_account.getAsJsonObject().get("email").getAsString();
+            String name = kakao_account.getAsJsonObject().get("profile").getAsJsonObject().get("nickname").getAsString();
 
-
-            userInfo.put("userid", userid);
-            if(email!=null){
-                userInfo.put("email" , email.getAsString());
-            }else{
-                userInfo.put("email" , "");
-            }
-
+            userInfo.put("userId", userId);
+            userInfo.put("name",name);
 
         } catch (IOException e) {
             e.printStackTrace();
