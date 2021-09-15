e7eea6c63be1311fdb99227f43784ee97af47304
Daun jo <jodawooooon@naver.com>
2021-09-15 16:12:29 +0900

    [#7] wip: hdfs 환경설정 재수정

--- backend/src/main/java/com/ssafy/config/HdfsConfig.java
+++ backend/src/main/java/com/ssafy/config/HdfsConfig.java
@@ -1,2 +1,21 @@
-package com.ssafy.config;public class HdfsConfig {
+package com.ssafy.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class HdfsConfig {
+
+    @Value("${hdfs.configuration.server}") String hdfsServer;
+    @Value("${hdfs.configuration.user}") String hdfsUser;
+
+    @Bean
+    public org.apache.hadoop.conf.Configuration config(){
+        System.setProperty("HADOOP_USER_NAME", hdfsUser);
+        org.apache.hadoop.conf.Configuration config = new org.apache.hadoop.conf.Configuration(true);
+        config.set("fs.defaultFS", hdfsServer);
+        return config;
+    }
+
 }
