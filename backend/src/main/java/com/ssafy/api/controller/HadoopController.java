4762b9afeb3e4e0580df903a7f236971ef69f3c4
Daun jo <jodawooooon@naver.com>
2021-09-15 16:12:05 +0900

    [#7] wip: test용 hadoop controller 구현

--- backend/src/main/java/com/ssafy/api/controller/HadoopController.java
+++ backend/src/main/java/com/ssafy/api/controller/HadoopController.java
@@ -1,33 +1,38 @@
 package com.ssafy.api.controller;
 
 
-import com.ssafy.api.service.HadoopDAO2;
+import com.ssafy.api.service.HdfsApiService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.ModelAndView;
 
 import java.util.Date;
 
 @Controller
 public class HadoopController {
-    HadoopDAO2 dao;
+
+    @Autowired
+    HdfsApiService dao;
+
     public HadoopController() {
         System.out.println("HadoopController2");
     }
-    @RequestMapping("/hadoophdfs")
-    public ModelAndView put(String action){
+    @RequestMapping("/hadoophdfs/{action}")
+    public String put( @PathVariable("action") String action){
         String result = "";
         if(action.equals("put"))
             result = dao.write("하둡 입력을 테스트 합니다...\n"+
                     new Date());
         else if(action.equals("get"))
-            result = dao.read("/ja605/message.txt");
+            result = dao.read("a.txt");
         else if(action.equals("delete"))
             result = dao.delete();
-
-        ModelAndView mav = new ModelAndView();
-        mav.addObject("hadooprw", result);
-        mav.setViewName("hadoopView");
-        return mav;
+//
+//        ModelAndView mav = new ModelAndView();
+//        mav.addObject("hadooprw", result);
+//        mav.setViewName("hadoopView");
+        System.out.println(result);
+        return result;
     }
 }
