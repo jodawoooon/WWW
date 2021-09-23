06e5cf897bc4879731c7c7c4926a105cd8d5168f
gyulgyul912 <gikim0565@naver.com>
2021-09-23 11:25:48 +0900

    [#16] feat: 위치 기반 편의점 정보 저장

--- crawling/test.py
+++ crawling/test.py
@@ -2,70 +2,116 @@ import pandas as pd
 import requests
 import json
 
-dong = pd.read_csv("C:/Users/multicampus/Desktop/seoulDongData.csv",encoding='euc-kr')
+dong = pd.read_csv(
+    "C:/Users/multicampus/Desktop/seoulDongData.csv", encoding='euc-kr')
 
+f = open('C:/Users/multicampus/Desktop/output.txt', 'w')
 
+addr_name = []
+plc_name = []
 corX = []
 corY = []
-for addr in dong :
+cnt = 1
+for addr in dong:
     address = addr
     url = "https://dapi.kakao.com/v2/local/search/address.json?query="+address
-    headers = {'Authorization' : 'KakaoAK 890a8bd3341e097d41c34c82eb9ed922'}
-    result = json.loads(str(requests.get(url,headers = headers).text))
+    headers = {'Authorization': 'KakaoAK 890a8bd3341e097d41c34c82eb9ed922'}
+    result = json.loads(str(requests.get(url, headers=headers).text))
 
-    x = result['documents'][0]['address']['x'] # 경도
-    y = result['documents'][0]['address']['y'] # 위도
+    x = result['documents'][0]['address']['x']  # 경도
+    y = result['documents'][0]['address']['y']  # 위도
 
     convs = {}
 
     # 편의점 위치
-    for i in range(1,46) :
-        headers = {'Authorization' : 'KakaoAK 890a8bd3341e097d41c34c82eb9ed922'}
-
+    for i in range(1, 46):
+        headers = {'Authorization': 'KakaoAK 890a8bd3341e097d41c34c82eb9ed922'}
         # 파라미터 가져오기
         params = {
-            'x' : float(x),
-            'y' : float(y),
-            'radius' : 10000, # 범위 0 ~20000까지 단위는 미터
-            'page' : i,
-            'size' : 15,
-            'sort' : 'distance'
+            'x': float(x),
+            'y': float(y),
+            'radius': 10000,  # 범위 0 ~20000까지 단위는 미터
+            'page': i,
+            'size': 15,
+            'sort': 'distance'
         }
 
         # 카테고리 키워드
         keywords = '편의점'
 
         # API 접근
-        url = 'https://dapi.kakao.com/v2/local/search/keyword.json?query={}'.format(keywords)
-        places = requests.get(url,headers = headers , params = params).json()['documents']
-
+        url = 'https://dapi.kakao.com/v2/local/search/keyword.json?query={}'.format(
+            keywords)
+        places = requests.get(url, headers=headers, params=params).json()[
+            'documents']
         # 빈 배열이면 stop
-        if places==[] :
+        if places == []:
             break
-        else :
+        else:
             convs[i] = places
+    address_name = []
+    place_name = []
     cox = []
     coy = []
 
     # 편의점 경위도 cox, coy에 순차적으로 저장해서 아래의 corX , corY에 넣기 위한 작업
-    for i in range(1, len(convs)) :
-        for a in range(len(convs[i])) :
+    for i in range(1, len(convs)):
+        for a in range(len(convs[i])):
+
+            add = convs[i][a]['address_name']
+            plc = convs[i][a]['place_name']
+            address_name.append(add)
+            place_name.append(plc)
             cox.append(convs[i][a]['x'])
             coy.append(convs[i][a]['y'])
-    
-    # 계속 리스트에 저장
-    corX+=list(set(cox))
-    corY+=list(set(coy))
 
+    # for i in range(1, len(convs)):
+    #     for a in range(len(convs[i])):
+    #         for key, value in convs[i][a].items():
+    #             if key == 'address_name' or key == 'category_group_name' or key == 'distance' or key == 'place_name' or key == 'place_url' or key == 'road_address_name' or key == 'x' or key == 'y':
+    #                 cox.append(key+" : "+value)
+            # corX.append(convs[i][a]['category_group_name'])
+            # corX.append(convs[i][a]['distance'])
+            # corX.append(convs[i][a]['place_name'])
+            # corX.append(convs[i][a]['place_url'])
+            # corX.append(convs[i][a]['road_address_name'])
+            # corX.append(convs[i][a]['x'])
+            # corX.append(convs[i][a]['y'])
+
+        # f.write('{'+str(cnt)+" ")
+        # for key, value in convs[i][a].items():
+        #     f.write(key+" : "+value+" ")
+
+        # f.write('}')
+        # cnt += 1
+        # f.write('{'+convs[i][a]['address_name']+',')
+        # f.write(convs[i][a]['category_group_name']+',')
+        # f.write(convs[i][a]['distance']+',')
+        # f.write(convs[i][a]['place_url']+',')
+        # f.write(convs[i][a]['road_address_name']+',')
+        # f.write(convs[i][a]['x']+',')
+        # f.write(convs[i][a]['y']+'}')
+
+    # 계속 리스트에 저장
+    # newInfo = []
+    # newInfo = list(set(cox))
+    # for i in newInfo:
+    #     print(i)
+    addr_name += list((address_name))
+    plc_name += list((place_name))
+    corX += list((cox))
+    corY += list((coy))
+    # f.write(corX)
+# f.close()
+# print(cnt)
 # 빈 데이터 프레임 생성해서 column에 저장
 place = pd.DataFrame()
-place['lon'] = corX
-place['lat'] = corY
+place['address'] = addr_name
+place['place'] = plc_name
+place['X'] = corX
+place['Y'] = corY
+# place['info'] = corX
 
-#csv 파일로 내보내기
-place.to_csv('C:/Users/multicampus/Desktop/output.csv')
 
-
-
-                     
-
+# csv 파일로 내보내기
+place.to_csv('C:/Users/multicampus/Desktop/out.csv', encoding='CP949')
