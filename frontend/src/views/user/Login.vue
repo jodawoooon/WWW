990175b9ba29a88e1c3d8592649142d9e7624cad
hyewon <hiii456@daum.net>
2021-09-27 10:32:18 +0900

    [#19] style: Login 프론트 구현

--- frontend/src/views/user/Login.vue
+++ frontend/src/views/user/Login.vue
@@ -1,19 +1,77 @@
 <template>
     <div>
-        <ComponentNav title="로그인"></ComponentNav>
+        <div class="login-content">
+            <div style="top-margin: 50px"><h2>환영합니다! 🙌</h2>
+            <p>나의 산책 친구, <br> <span class="title">WWW</span> 와 함께 걸어보세요!</p></div>
+        </div>
+
+        <KakaoLogin
+            class="kakao"
+            api-key="779f3000dd215fa0e783546831836eca"
+            image="kakao_login_btn_medium"
+            :on-success=onSuccess
+            :on-failure=onFailure
+            />
     </div>
+
 
 </template>
 
 <script>
-import ComponentNav from "@/components/common/ComponentNav";
+import KakaoLogin from 'vue-kakao-login'
+import("@/assets/style/Main.css");
 
+let onSuccess = (data) => {
+  console.log(data)
+  console.log("Login success")
+}
+let onFailure = (data) => {
+  console.log(data)
+  console.log("Login failure")
+}
 
 export default {
     name: 'Login',
     components: {
-        ComponentNav
+        KakaoLogin
     },
-
+    methods: {
+        onSuccess,
+        onFailure
+    }
 }
 </script>
+
+<style scoped>
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
+}
+#navBar {
+  position: fixed;
+  width: 100%;
+  max-width: 425px;
+  left: 0;
+  right: 0;
+  margin: 0 auto;
+  height: 60px;
+  z-index: 3;
+}
+.kakao{
+    margin-top:3%;
+}
+.login-content{
+    text-align: left;
+}
+.title{
+    font-weight: 900;
+}
+</style>
