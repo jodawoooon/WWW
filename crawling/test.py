d5ffb0e42c89fce52615a0877376e7545cc1a22b
gyulgyul912 <gikim0565@naver.com>
2021-09-28 09:14:55 +0900

    [#16] feat: 중복제거

--- crawling/test.py
+++ crawling/test.py
@@ -3,21 +3,21 @@ import requests
 import json
 
 dong = pd.read_csv(
-    "C:/Users/multicampus/Desktop/seoulDongData.csv", encoding='euc-kr')
+    "C:/Users/multicampus/Desktop/전국동이름.csv", encoding='euc-kr')
 
 f = open('C:/Users/multicampus/Desktop/output.txt', 'w')
-
 addr_name = []
 plc_name = []
 corX = []
 corY = []
 cnt = 1
-for addr in dong:
-    address = addr
+for b in dong.index:
+    address = dong["name"][b]
+    if not address:
+        break
     url = "https://dapi.kakao.com/v2/local/search/address.json?query="+address
     headers = {'Authorization': 'KakaoAK 890a8bd3341e097d41c34c82eb9ed922'}
     result = json.loads(str(requests.get(url, headers=headers).text))
-
     x = result['documents'][0]['address']['x']  # 경도
     y = result['documents'][0]['address']['y']  # 위도
 
@@ -44,18 +44,28 @@ for addr in dong:
             keywords)
         places = requests.get(url, headers=headers, params=params).json()[
             'documents']
+        # print(places)
+
+        if i > 1:
+            if convs[i-1][0] == places[0]:
+                break
         # 빈 배열이면 stop
         if places == []:
             break
         else:
             convs[i] = places
+        # convs = places
+
+        if len(places) < 15:
+            break
+        # print(convs[i][1])
     address_name = []
     place_name = []
     cox = []
     coy = []
 
     # 편의점 경위도 cox, coy에 순차적으로 저장해서 아래의 corX , corY에 넣기 위한 작업
-    for i in range(1, len(convs)):
+    for i in range(1, len(convs)+1):
         for a in range(len(convs[i])):
 
             add = convs[i][a]['address_name']
@@ -70,13 +80,13 @@ for addr in dong:
     #         for key, value in convs[i][a].items():
     #             if key == 'address_name' or key == 'category_group_name' or key == 'distance' or key == 'place_name' or key == 'place_url' or key == 'road_address_name' or key == 'x' or key == 'y':
     #                 cox.append(key+" : "+value)
-            # corX.append(convs[i][a]['category_group_name'])
-            # corX.append(convs[i][a]['distance'])
-            # corX.append(convs[i][a]['place_name'])
-            # corX.append(convs[i][a]['place_url'])
-            # corX.append(convs[i][a]['road_address_name'])
-            # corX.append(convs[i][a]['x'])
-            # corX.append(convs[i][a]['y'])
+        # corX.append(convs[i][a]['category_group_name'])
+        # corX.append(convs[i][a]['distance'])
+        # corX.append(convs[i][a]['place_name'])
+        # corX.append(convs[i][a]['place_url'])
+        # corX.append(convs[i][a]['road_address_name'])
+        # corX.append(convs[i][a]['x'])
+        # corX.append(convs[i][a]['y'])
 
         # f.write('{'+str(cnt)+" ")
         # for key, value in convs[i][a].items():
