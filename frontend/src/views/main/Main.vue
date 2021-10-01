8b93658ac3d4f6e0f4dcbe55d5d0b8d79572ec6c
Daun jo <jodawooooon@naver.com>
2021-10-01 14:34:18 +0900

    [#20] wip: 위치추적 동의 안했을 시 index로 push하기 적용

--- frontend/src/views/main/Main.vue
+++ frontend/src/views/main/Main.vue
@@ -112,7 +112,7 @@ export default {
         (pos) => {
           this.lat = pos.coords.latitude;
           this.lng = pos.coords.longitude;
-
+          this.$store.commit("SET_IS_AGREE");
           axios
             .get(
               "https://dapi.kakao.com/v2/local/geo/coord2regioncode.json?x=" +
@@ -135,7 +135,9 @@ export default {
             });
         },
         (err) => {
-          this.textContent = err.message;
+          console.log(err);
+          this.$store.commit("SET_IS_NOT_AGREE");
+          router.push("/index");
         }
       );
     },
