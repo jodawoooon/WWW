9eee8f1c5e4102e018647bd8b32b94fb3fc46c57
topgun0209 <topgun0209@naver.com>
2021-10-06 10:41:47 +0900

    env test

--- frontend/src/views/walk/Record.vue
+++ frontend/src/views/walk/Record.vue
@@ -176,7 +176,7 @@ export default {
       /* global kakao */
       script.onload = () => kakao.maps.load(this.initMap);
       script.src =
-        "//dapi.kakao.com/v2/maps/sdk.js?appkey=779f3000dd215fa0e783546831836eca&autoload=false";
+        "//dapi.kakao.com/v2/maps/sdk.js?appkey=%VUE_APP_NEWS_KEY%&autoload=false";
       document.head.appendChild(script);
       this.resetLocations();
       this.accumulated_distance = 0;
